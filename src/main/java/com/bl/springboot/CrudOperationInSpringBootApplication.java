7dac26a669c598faaee40352794cd4940cf682a7
Tushar-W <twankhede95@gmail.com>
2020-06-23 17:31:30 +0530

    ADDED:spring boot dependencies in pom.xml file

---
+++ src/main/java/com/bl/springboot/CrudOperationInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bl.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationInSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudOperationInSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bl/springboot/CrudOperationInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationInSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
