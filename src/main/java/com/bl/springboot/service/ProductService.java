bdee450254f255a62c9adf28df78371b18c755d0
Tushar-W <twankhede95@gmail.com>
2020-06-24 10:03:23 +0530

    ADDED:create functionality to delete product details by id

--- src/main/java/com/bl/springboot/service/ProductService.java
+++ src/main/java/com/bl/springboot/service/ProductService.java
@@ -3,7 +3,6 @@ package com.bl.springboot.service;
 import com.bl.springboot.model.Product;
 import com.bl.springboot.repository.IProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
 
 public class ProductService {
@@ -23,5 +22,7 @@ public class ProductService {
         return repo.findById(id).get();
     }
 
-
+    public void delete(Integer id){
+        repo.deleteById(id);
+    }
 }
